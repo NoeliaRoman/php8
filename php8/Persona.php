a9638ec402422ef24c3d3249298959096a3f00e4
NoeliaRoman <noelia.roman99@hotmail.com>
2021-10-04 19:21:25 -0700

    se inicializo el git

---
+++ php8/Persona.php
@@ -0,0 +1,9 @@
+<?php
+
+class Persona{
+
+    public function saludar(){
+        echo 'saludar';
+    }
+};
+?>
---
+++ php8/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'Persona.php';
+$persona1 = new $persona1;
+
+?>
